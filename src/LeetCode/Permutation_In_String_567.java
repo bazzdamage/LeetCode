37a2450ec45965ce75770e09e215c88d62f926d5
bazzdamage <bazzdamage@outlook.com>
2022-08-22 22:38:34 +0700

    fix in 567

--- src/LeetCode/Permutation_In_String_567.java
+++ src/LeetCode/Permutation_In_String_567.java
@@ -19,21 +19,17 @@ public class Permutation_In_String_567 {
         int end = s1chars.length;
 
         for (Character c : s1chars) {
-            if (s1map.containsKey(c)) {
-                s1map.put(c, s1map.get(c) + 1);
-            } else {
-                s1map.put(c, 1);
-            }
+            s1map.putIfAbsent(c, 0);
+            s1map.put(c, s1map.get(c) + 1);
         }
         while (end <= s2chars.length) {
             for (int i = start; i < end; i++) {
-                if (temp.containsKey(s2chars[i])) {
-                    temp.put(s2chars[i], temp.get(s2chars[i]) + 1);
-                } else {
-                    temp.put(s2chars[i], 1);
-                }
+                temp.putIfAbsent(s2chars[i], 0);
+                temp.put(s2chars[i], temp.get(s2chars[i]) + 1);
             }
-            if (s1map.equals(temp)) return true;
+            if (s1map.equals(temp)) {
+                return true;
+            }
             temp.clear();
             start++;
             end++;
